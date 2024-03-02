a6463ae2cc34907dd0ec888cf0223b5eee353c85
Constanza San Miguel <csanmiguel49@gmail.com>
2024-03-02 19:32:18 -0300

    CustomRouter + policies

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import MongoStore from "connect-mongo"
 //import users from "./src/data/fs/userFsManager.js"
 import socketUtils from "./src/utils/socket.utils.js"
 
-import router from "./src/routers/index.router.js"
+import IndexRouter from "./src/routers/index.router.js"
 import errorHandler from "./src/middlewares/errorHandler.js"
 import pathHandler from "./src/middlewares/pathHandler.js"
 import __dirname from "./utils.js"
@@ -83,7 +83,8 @@ server.use(express.static(__dirname + "/public"))
 server.use(morgan("dev"))
 
 //endpoints
-server.use("/", router)
+const router = new IndexRouter()
+server.use("/", router.getRouter())
 server.use(errorHandler)
 server.use(pathHandler)
 
--- src/routers/api/index.api.js
+++ src/routers/api/index.api.js
@@ -1,14 +1,17 @@
-import { Router } from "express"
+import CustomRouter from "../CustomRouter.js"
 import usersRouter from "./user.router.api.js"
-import productsRouter from "./products.router.api.js"
+import ProductsRouter from "./products.router.api.js"
 import ordersRouter from "./orders.router.api.js"
 import sessionsRouter from "./sessions.router.api.js"
+import passCallBack from "../../middlewares/passCallBack.js"
 
-const apiRouter = Router()
+const product = new ProductsRouter()
 
-apiRouter.use("/users", usersRouter)
-apiRouter.use("/products", productsRouter)
-apiRouter.use("/orders", ordersRouter)
-apiRouter.use("/sessions", sessionsRouter)
-
-export default apiRouter
+export default class ApiRouter extends CustomRouter {
+    init() {
+        this.router.use("/users", usersRouter)
+        this.router.use("/products", product.getRouter())
+        this.router.use("/orders", passCallBack("jwt"), ordersRouter)
+        this.router.use("/sessions", sessionsRouter)
+    }
+}
